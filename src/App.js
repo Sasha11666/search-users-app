c7911ac6384e8a31f8656bee382d575c78ad35f1
Sasha11666 <basovasasa087@gmail.com>
2023-12-27 04:32:08 -0500

    added functionality and design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import "./App.css";
+import { Search } from "./components/search/Search";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+*:before,
+*:after {
+  -webkit-box-sizing: border-box;
+   box-sizing: border-box;
+}
+
+
+html,
+body {
+  font-family: 'RobotoRegular', sans-serif;
+}
+
+
+@font-face {
+  font-family: "RobotoRegular";
+  src: local("RobotoRegular"), url(./fonts/Roboto-Regular.ttf) format(truetype);
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "RobotoMedium";
+  src: local("RobotoMedium"), url(./fonts/Roboto-Medium.ttf) format(truetype);
+  font-weight: 500;
+}
+}`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,21 @@
+export async function getUsers(login) {
+  const response = await fetch(
+    `https://api.github.com/search/users?q=${login}&sort=repositories&order=desc`
+  );
+
+  if (!response.ok) {
+    throw new Error("Ошибка сервера");
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function getUserInfo(url) {
+  const response = await fetch(`${url}`);
+
+  if (!response.ok) {
+    throw new Error("Ошибка сервера");
+  }
+  const data = await response.json();
+  return data;
+}
